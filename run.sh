649551a21fbd719498d987f3a87220c638930f65
ogontaro <ogontaro@gmail.com>
2018-09-04 02:26:32 +0900

    refactor run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 DOCKER_CONTAINER=sitespeedio/sitespeed.io:7.3.6
 
 # Setup the network and default ones we wanna use
-sudo /home/ubuntu/startNetworks.sh
+sudo /home/ubuntu/sitespeed.io/startNetworks.sh
 THREEG="--network 3g"
 CABLE="--network cable"
 
@@ -13,12 +13,12 @@ DOCKER_SETUP="--shm-size=1g --rm -v /home/ubuntu/sitespeed.io/config:/sitespeed.
 
 # Start running the tests
 # We run more tests on our test server but this gives you an idea of how you can configure it
-docker run $CABLE $DOCKER_SETUP $DOCKER_CONTAINER -n 11 --browsertime.viewPort 1920x1080 --browsertime.cacheClearRaw --browsertime.chrome.collectTracingEvents /sitespeed.io/wikipedia.org.txt $CONFIG
+docker run $CABLE $DOCKER_SETUP $DOCKER_CONTAINER -n 11 --browsertime.viewPort 1920x1080 --browsertime.cacheClearRaw --browsertime.chrome.collectTracingEvents urls.txt $CONFIG
 docker run $CABLE $DOCKER_SETUP $DOCKER_CONTAINER -n 11 --browsertime.viewPort 1920x1080 /sitespeed.io/wikipedia.org.txt -b firefox $CONFIG
 docker run $THREEG $DOCKER_SETUP $DOCKER_CONTAINER --graphite.namespace sitespeed_io.emulatedMobile --browsertime.chrome.collectTracingEvents /sitespeed.io/m.wikipedia.org.txt -c 3g --mobile true $CONFIG
 
 # We remove all docker stuff to get a clean next run
-docker system prune --all --volumes -f
+#docker system prune --all --volumes -f
 
 # Get the container so we have it the next time we wanna use it
 docker pull $DOCKER_CONTAINER
