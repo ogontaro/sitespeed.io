1d3df61cf574492fbe3aa8287cf624dfff0f4218
ogontaro <ogontaro@gmail.com>
2018-09-06 23:53:49 +0900

    refactor

--- run.sh
+++ run.sh
@@ -9,11 +9,11 @@ CABLE="--network cable"
 
 # Simplify some configurations
 CONFIG="--config default.json"
-DOCKER_SETUP="--shm-size=1g --rm -v /home/ubuntu/sitespeed.io/config:/sitespeed.io -v /home/ubuntu/sitespeed.io/result:/result -v /etc/localtime:/etc/localtime:ro --name sitespeed"
+DOCKER_SETUP="--shm-size=1g --rm -v /home/ubuntu/sitespeed.io:/sitespeed.io -v /home/ubuntu/sitespeed.io/result:/result -v /etc/localtime:/etc/localtime:ro --name sitespeed"
 
 # Start running the tests
 # We run more tests on our test server but this gives you an idea of how you can configure it
-docker run $CABLE $DOCKER_SETUP $DOCKER_CONTAINER -n 11 --browsertime.viewPort 1920x1080 urls.txt $CONFIG
+docker run $CABLE $DOCKER_SETUP $DOCKER_CONTAINER -n 11 --browsertime.viewPort 1920x1080 --browsertime.chrome.args ignore-certificate-errors urls.txt $CONFIG
 docker run $CABLE $DOCKER_SETUP $DOCKER_CONTAINER -n 11 --browsertime.viewPort 1920x1080 urls.txt -b firefox $CONFIG
 docker run $THREEG $DOCKER_SETUP $DOCKER_CONTAINER --graphite.namespace sitespeed_io.emulatedMobile url.txt -c 3g --mobile true $CONFIG
 
